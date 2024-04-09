5f94fba958dcb5662db8c4f6ff8c1dbf4013802a
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-04-09 16:01:50 -0300

    feat: toggle theme and account menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { router } from "./routes";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors closeButton />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
